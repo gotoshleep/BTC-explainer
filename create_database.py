5386015a460da072b31028ac85e28394be7f4bcd
Ashley Wong <ashleyprw@gmail.com>
2025-09-22 00:28:19 +0800

    created code for chroma_db

--- create_database.py
+++ create_database.py
@@ -18,7 +18,7 @@ load_dotenv()
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 CHROMA_PATH = "chroma"
-DATA_PATH = "data/bitcoin.md"
+DATA_PATH = "data/"
 
 
 def main():
@@ -71,5 +71,3 @@ if __name__ == "__main__":
     main()
 
 
-
-print(DATA_PATH)
